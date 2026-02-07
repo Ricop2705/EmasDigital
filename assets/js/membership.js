da0de9d8e8c1955ec18a03d820b9c2299607ace8
Ricop2705 <ricop.1802@gmail.com>
2026-02-07 23:45:43 +0900

    Update membership.js

--- assets/js/membership.js
+++ assets/js/membership.js
@@ -35,25 +35,3 @@ function closeSignup(){
 /* expose ke HTML */
 window.openSignup = openSignup;
 window.closeSignup = closeSignup;
-
-/* ===============================
-   REAL AUTH STATE ENGINE
-================================ */
-
-function isLoggedIn(){
-  return localStorage.getItem("memberType") !== null;
-}
-
-function applyAuthUI(){
-
-  const loginBtn = document.querySelector(".login-link");
-  const memberBtn = document.querySelector(".member-link");
-
-  if(isLoggedIn()){
-    document.body.classList.add("user-logged");
-    if(loginBtn) loginBtn.innerText="Dashboard";
-    if(memberBtn) memberBtn.style.display="inline-block";
-  }
-}
-
-window.addEventListener("load",applyAuthUI);
